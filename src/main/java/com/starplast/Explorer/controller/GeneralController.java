87dbf3289b47ec8fbd69d7516c08946860161c5f
rkengoum <rodrigue.kengoum-gassam@capgemini.com>
2023-07-03 11:09:43 +0200

    update back 1

--- src/main/java/com/starplast/Explorer/controller/GeneralController.java
+++ src/main/java/com/starplast/Explorer/controller/GeneralController.java
@@ -1,12 +1,28 @@
 package com.starplast.Explorer.controller;
 
 
+import com.starplast.Explorer.models.Fichier;
+import com.starplast.Explorer.models.Response.PDF;
+import jakarta.annotation.Resource;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import com.starplast.Explorer.models.Directory;
 import com.starplast.Explorer.services.GeneralService;
+
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 @Controller
 @CrossOrigin(origins = "http://localhost:4200")
